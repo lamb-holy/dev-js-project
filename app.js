5cc6d73ac1c6f115b164482d80f2b215c7ffcca3
lamb-holy <daryna.shymchuk@gmail.com>
2026-06-13 11:31:38 +0300

    i.h.b.p.f.j.a.s.t.m.n.e!

---
+++ app.js
@@ -0,0 +1,12 @@
+// Етап 2
+var studioName = "";
+
+do {
+    studioName = prompt("Введіть назву вашої студії");
+} while (studioName.length < 3);
+
+var gameName = "";
+
+do {
+    gameName = prompt("Як називатиметься ваша гра?");
+} while (gameName.length < 2);
