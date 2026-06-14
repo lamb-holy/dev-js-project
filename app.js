bc9908e8e86547725835192d679d999b9f4258aa
lamb-holy <daryna.shymchuk@gmail.com>
2026-06-14 12:36:45 +0300

    fine.......................

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 // Етап 2
-var studioName = "";
 
-do {
-    studioName = prompt("Введіть назву вашої студії");
-} while (studioName.length < 3);
+// назва студії
+var studioName = prompt("Введіть назву вашої GameDev студії");
 
-var gameName = "";
+while (studioName.length < 3) {
+    studioName = prompt("Замало символів. Введіть назву ще раз");
+}
 
-do {
-    gameName = prompt("Як називатиметься ваша гра?");
-} while (gameName.length < 2);
+// назва гри
+var gameName = prompt("Яку гру ви хочете створити?");
+
+while (gameName.length < 2) {
+    gameName = prompt("Замало символів. Введіть назву гри ще раз.");
+}
+
+// бюджет
+var moneyMoney = Number(prompt("Який у вас бюджет?"));
+
+while (isNaN(moneyMoney)) {
+    moneyMoney = Number(prompt("Помилка: використовуйте лише цифри."));
+}
+
+alert(`Ваш бюджет: $${moneyMoney}`);
+
+// термін
